f838f7396b7d9531761c764a3db5e93bf2e5380c
nathy-melo <natymelo2008@gmail.com>
2025-06-04 10:32:26 -0300

    mudancas2

--- processamento.php
+++ processamento.php
@@ -27,11 +27,11 @@
             echo "<p><strong>Interesses:</strong> Nenhum interesse selecionado</p>";
         };
 
-        $formatos_permitidos = array("jpg", "gif", "bmp", "png");
-
-        if (in_array($arquivo['type'], $formatos_permitidos)){
-            echo "<p>Foto de perfil escolhida!</p>";
-            echo "<img>". implode($arquivo) . "</img>";
+        $formatos_permitidos = array("image/jpg", "image/gif", "image/bmp", "image/png");
+        move_uploaded_file($arquivo['tmp_name'], "formulario-e-upload-de-imagens/" .$arquivo["name"])
+        if (in_array($arquivo['type'],  $formatos_permitidos)){//error
+            echo "<p>Foto de perfil escolhida!</p>"; 
+            echo "<img src= ". ($arquivo) ."></img>";
         } else {
             echo "<p>Formato de arquivo não permitido!</p>";
         }
